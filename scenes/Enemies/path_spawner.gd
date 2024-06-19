02273936246ba7bcc5d88bddd43806769fb7feab
Ben Toosey <41492238+btoosey@users.noreply.github.com>
2024-06-19 22:38:34 +0100

    ADD: Enemy spawner

--- scenes/Enemies/path_spawner.gd
+++ scenes/Enemies/path_spawner.gd
@@ -1,9 +1,7 @@
 extends Path2D
 
-@onready var path_follow_2d = $PathFollow2D
-const ENEMY = preload("res://scenes/Enemies/enemy.tscn")
-
+const ENEMY_FOLLOW_PATH_1 = preload("res://scenes/Enemies/Follow Paths/enemy_follow_path_1.tscn")
 
 func spawn_enemy() -> void:
-	var new_enemy = ENEMY.instantiate()
-	path_follow_2d.add_child(new_enemy)
+	var new_enemy = ENEMY_FOLLOW_PATH_1.instantiate()
+	add_child(new_enemy)
